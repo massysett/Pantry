78fc51a74003e7b38805c100578098299378cf01
Omari Norman <omari@smileystation.com>
2011-11-05 18:34:30 -0400

    add functions

--- Food.hs
+++ Food.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Food where
 
 import qualified Data.Map as M
@@ -7,21 +8,22 @@ import Text.Regex.PCRE
 import Data.Maybe
 
 data Nuts = Nuts (M.Map String Rational) deriving Show
-data Units = Units(M.Map String Rational) deriving Show
+data Units = Units(M.Map String Grams) deriving Show
 data Tags = Tags (M.Map String String) deriving Show
-data MixedNum = MixedNum Decimal Rational deriving Show
-data AbsUnit = Grams | Ounces | Pounds deriving Show
+data MixedNum = MixedNum Decimal Rational deriving (Eq, Show)
+data AbsUnit = AbsGrams | Ounces | Pounds deriving Show
 data CurrUnit = Absolute AbsUnit
               | Arbitrary Unit
               deriving Show
 data PctRefuse = PctRefuse Decimal deriving Show
-data Qty = Qty MixedNum deriving Show
+newtype Qty = Qty MixedNum deriving (Eq, Ord, Num, Real, Show)
 data Yield = Yield (Maybe Decimal) deriving Show
 data Ingr = Ingr [Food] deriving Show
 
 data Nut = Nut String Rational deriving Show
-data Unit = Unit String Rational deriving Show
+data Unit = Unit String Grams deriving Show
 data Tag = Tag String String deriving Show
+newtype Grams = Grams Rational deriving (Eq, Ord, Show, Num, Real)
 
 data Food = Food { tags :: Tags
                  , units :: Units
@@ -36,7 +38,7 @@ emptyFood :: Food
 emptyFood = Food { tags = Tags M.empty
                  , units = Units M.empty
                  , nuts = Nuts M.empty
-                 , currUnit = Absolute Grams
+                 , currUnit = Absolute AbsGrams
                  , pctRefuse = PctRefuse (Decimal 0 0)
                  , qty = Qty (MixedNum (Decimal 0 100) (0 % 1))
                  , yield = Yield Nothing
@@ -90,7 +92,7 @@ allUnitsMap (Units m) = M.fromList eithers where
   arbKeys = M.keys m
   arbVals = map Right . map (uncurry Unit) $ M.assocs m
   absKeys = ["g", "oz", "lb"]
-  absVals = map Left [Grams, Ounces, Pounds]
+  absVals = map Left [AbsGrams, Ounces, Pounds]
   abss = zip absKeys absVals
 
 if' :: Bool -> a -> a -> a
@@ -109,6 +111,45 @@ changeCurrUnit p f = if' oneMatch (Right updateFood) (Left err) where
   err = if null matches
         then NoMatchingUnit
         else MultipleMatchingUnits matches
+
+-- Nuts manipulations
+getNuts :: Food -> [Nut]
+getNuts f = map (uncurry Nut) (M.assocs m) where
+  (Nuts m) = nuts f
+
+instance Num MixedNum where
+  (+) (MixedNum d1 r1) (MixedNum d2 r2) =
+    MixedNum (d1 + d2) (r1 + r2)
+  (-) (MixedNum d1 r1) (MixedNum d2 r2) =
+    MixedNum (d1 - d2) (r1 - r2)
+  (*) (MixedNum d1 r1) (MixedNum d2 r2) =
+    MixedNum (d1 * d2) (r1 * r2)
+  abs (MixedNum d r) = MixedNum (abs d) (abs r)
+  signum (MixedNum d r) = if' (s == 0) zero signed where
+    zero = MixedNum (Decimal 0 0) (0 % 1)
+    signed = if' (s < 0) negative positive
+    negative = MixedNum (Decimal 0 (-1)) (0 % 1)
+    positive = MixedNum (Decimal 0 1) (0 % 1)
+    s = toRational d + r
+  fromInteger i = MixedNum (fromInteger i) (0 % 1)
+
+instance Ord MixedNum where
+  (<=) (MixedNum d1 r1) (MixedNum d2 r2) =
+    (toRational d1 + r1) <= (toRational d2 + r2)
+
+instance Real MixedNum where
+  toRational (MixedNum d r) = toRational d + r
+
+absUnitGrams :: AbsUnit -> Grams
+absUnitGrams AbsGrams = Grams $ 1 % 1
+absUnitGrams Ounces = Grams $ 2835 % 100
+absUnitGrams Pounds = Grams $ 4536 % 10
+
+foodGrams :: Food -> Grams
+foodGrams f = Grams $ toRational (qty f) * toRational grams where
+  grams = case (currUnit f) of
+    (Absolute abs) -> absUnitGrams abs
+    (Arbitrary (Unit _ g)) -> g
 
 data Error = NoMatchingUnit
            | MultipleMatchingUnits [String]
