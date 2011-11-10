ad48fd07e504a058b38d0cc9e4f726ca05238da7
Omari Norman <omari@smileystation.com>
2011-11-10 09:06:25 -0500

    Express commit and push

--- SimpleParser.hs
+++ SimpleParser.hs
@@ -20,13 +20,30 @@ flagParser s ps = Right $ ps ++ [ParsedFlag s]
 singleParser :: String
                 -> [ParsedOpt]
                 -> String
-                -> String
                 -> Either SimpleErr [ParsedOpt]
-singleParser n ps s1 = Right $ ps ++ [ParsedSingle s1 s2]
+singleParser n ps s1 = Right $ ps ++ [ParsedSingle n s1]
 
 doubleParser :: String
                 -> [ParsedOpt]
                 -> String
                 -> String
+                -> Either SimpleErr [ParsedOpt]
+doubleParser n ps s1 s2 = Right $ ps ++ [ParsedDouble n s1 s2]
 
+makeFlagOpt :: [Char] -- ^ Short option names
+               -> [String] -- ^ Long option names
+               -> String -- ^ Name of this option for the [ParsedOpt] list
+               -> OptDesc [ParsedOpt] SimpleErr
+makeFlagOpt ss ls n = OptDesc ss ls (Flag $ flagParser n)
 
+makeSingleOpt :: [Char] -- ^ Short option names
+                 -> [String] -- ^ Long option names
+                 -> String -- ^ Name of this option for the [ParsedOpt] list
+                 -> OptDesc [ParsedOpt] SimpleErr
+makeSingleOpt ss ls n = OptDesc ss ls (Single $ singleParser n)
+
+makeDoubleOpt :: [Char] -- ^ Short option names
+                 -> [String] -- ^ Long option names
+                 -> String -- ^ Name of this option for the [ParsedOpt] list
+                 -> OptDesc [ParsedOpt] SimpleErr
+makeDoubleOpt ss ls n = OptDesc ss ls (Double $ doubleParser n)
