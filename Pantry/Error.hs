25f391cd46d3e6e6e1452a2dbf29d4729d3db4d4
Omari Norman <omari@smileystation.com>
2011-12-25 13:43:20 -0700

    add error messages

--- Pantry/Error.hs
+++ Pantry/Error.hs
@@ -1,14 +1,20 @@
 module Pantry.Error where
 
 import qualified Control.Monad.Error as E
-import Pantry.Food (UnitName, FoodId, SetQtyByNutFailure,
+import Pantry.Food (UnitName, FoodId,
+                    SetQtyByNutFailure(QBNNoMatchingNut,
+                                       QBNMultipleMatchingNuts,
+                                       QBNNutIsZero),
                     Food)
 import qualified Pantry.Food as F
 import Control.Exception ( IOException )
 import qualified Data.Text as X
 import Data.Word ( Word8 )
 import Pantry.Types (NonNegInteger)
+import qualified Pantry.Types as T
 import qualified System.Console.OptParse.OptParse as O
+import Pantry.Exact ( exact )
+import qualified Data.Map as M
 
 data Error = NotExactlyOneMatchingUnit [UnitName]
            | AddNutToZeroQty
@@ -58,12 +64,298 @@ instance O.ParseErr Error where
 showError :: Error -> X.Text
 showError e = case e of
   (NotExactlyOneMatchingUnit us) ->
-    message b ls e where
+    message b ls e Clean where
       b = "not exactly one matching unit"
       ls = [ "You entered a pattern to change the unit of a food,"
            , "but there was more than one unit matching the"
            , "pattern. Matching units:"
            ] ++ map (X.unpack . F.unUnitName) us
+  
+  (AddNutToZeroQty) ->
+    message b ls e Clean where
+      b = "attempted to add nutrient to food whose quantity is zero"
+      ls = [ "This cannot be done as Pantry cannot determine how to"
+           , "store the nutrient value."
+           ]
+  
+  (RegexComp s) ->
+    message b ls e Clean where
+      b = "could not compute regular expression"
+      ls = [ "The pattern you gave for a regular expression resulted"
+           , "in a compilation error. The error message was:"
+           , s
+           ]
+  
+  (NoReportMatch s ss) ->
+    message b ls e Clean where
+      b = "not exactly one matching report"
+      ls = [ "The string you gave for a report name does not match"
+           , "exactly one report name. String you gave:"
+           , s
+           , "Matching report names:" ]
+           ++ case ss of [] -> ["(none)"]
+                         ls -> ls
+  
+  (Other s) ->
+    message b ls e Dirty where
+      b = "an unknown error occurred"
+      ls = [ "This is a bug; please report it to"
+           , "omari@smileystation.com"
+           ]
+  
+  (MoveIdNotFound i) ->
+    message b ls e Clean where
+      b = "Food ID specified for move option not found"
+      ls = [ "You want to move a food, but the ID you specified was"
+           , "not found. ID you specified:"
+           , show i
+           ]
+  
+  (MultipleMoveIdMatches i) ->
+    message b ls e Clean where
+      b = "Food ID specified for move found multiple foods"
+      ls = [ "You want to move a food, but the ID you specified"
+           , "matched more than one food in volatile."
+           , "ID you specified:"
+           , show i
+           ]
+  
+  (MultipleEditIdMatches i) ->
+    message b ls e Clean where
+      b = "Edit option found multiple identical IDs in volatile"
+      ls = [ "The edit option requires that each Food ID in volatile"
+           , "be unique, but there are multiple foods with"
+           , "the same ID. ID that matches multiple foods:"
+           , show i
+           ]
+  
+  (MoveStartNotFound i) ->
+    message b ls e Clean where
+      b = "ID specified as basis for move option not found"
+      ls = [ "The first food ID you gave for the move option"
+           , "was not found. ID you specified:"
+           , show i
+           ]
+  
+  (CanonicalizeError x) ->
+    message b ls e Clean where
+      b = "Could not canonicalize a path"
+      ls = [ "Pantry converts all paths to canonical paths before"
+           , "loading or saving files. A path you gave could not be"
+           , "canonicalized. Make sure the path is valid."
+           , "Error given by the operating system:"
+           , show x
+           ]
+  
+  (FileSaveError x) ->
+    message b ls e Dirty where
+      b = "IO error occurred while saving a file to disk"
+      ls = [ "Error given by the operating system:"
+           , show x
+           ]
+
+  (FileReadError x) ->
+    message b ls e Clean where
+      b = "IO error occurred while reading a file from disk"
+      ls = [ "Error given by the operating system:"
+           , show x
+           ]
+  
+  (FileDecodeError s) ->
+    message b ls e Clean where
+      b = "Error occurred when decoding file contents"
+      ls = [ "All data was loaded from disk, but Pantry encountered"
+           , "an error when decoding the contents of the file."
+           , "Further description of the error:"
+           , s
+           ]
+  
+  (NotPantryFile) ->
+    message b [] e Clean where
+      b = "File name given for opening is not a Pantry file"
+  
+  (WrongFileVersion) ->
+    message b ls e Clean where
+      b = "File name given for opening is wrong version"
+      ls = [ "File name given is a Pantry file, but it is"
+           , "not compatible with this version of Pantry."
+           ]
+  
+  (NoSaveFilename) ->
+    message b ls e Clean where
+      b = "Save option used but there is no default filename"
+      ls = [ "Try using the save-as option instead." ]
+  
+  (UndoTooBig nni i) ->
+    message b ls e Clean where
+      b = "Number given for undo option is too large"
+      ls = [ "The number you gave for the undo option exceeds"
+           , "the number of undo levels currently stored."
+           , "Number you gave: " ++ (show . T.unNonNegInteger $ nni)
+           , "Number of levels stored: " ++ show i
+           ]
+  
+  (EmptyFilePath) ->
+    message b ls e Clean where
+      b = "Empty file path"
+      ls = [ "You used an option that requires a file path,"
+           , "but you passed an empty string."
+           ]
+  
+  (FindLoadFileError x) ->
+    message b ls e Clean where
+      b = "Could not find file to load"
+      ls = [ "The file name you gave to load a file could"
+           , "not be found. The error given by the"
+           , "operating system:"
+           , show x
+           ]
+  
+  (FindSaveDirError x) ->
+    message b ls e Clean where
+      b = "Could not find directory to save in"
+      ls = [ "When saving to disk Pantry must first locate the"
+           , "directory in which to save your file, but the"
+           , "directory could not be opened. The error given"
+           , "by the operating system:"
+           , show x
+           ]
+  
+  (IngrToVolatileLookup is) ->
+    message b ls e Clean where
+      b = "ingredients-to-volatile could not find a food"
+      ls = [ "One or more of the Food IDs given for the"
+           , "ingredients-to-volatile command could not be found"
+           , "in the buffer. IDs that were not found:"
+           ] ++ (map X.unpack . map exact $ is)
+  
+  (IngrFromVolatileNotFound is) ->
+    message b ls e Clean where
+      b = "ingredients-from-volatile could not find a food"
+      ls = [ "One or more of the Food IDs given for the"
+           , "ingredients-from-volatile command could not be found"
+           , "in the buffer. IDs that were not found:"
+           ] ++ (map X.unpack . map exact $ is)
+  
+  (ParseError s) ->
+    message b ls e Clean where
+      b = "Error while parsing the command line"
+      ls = [ "The error given by the command line parser:"
+           , s
+           ]
+  
+  (IDsNotFound is) ->
+    message b ls e Clean where
+      b = "IDs specified with the id command not found"
+      ls = [ "One or more of the Food IDs specified for the"
+           , "--id command not found. IDs not found:"
+           ] ++ (map X.unpack . map exact $ is)
+  
+  (IDStringNotValid s) ->
+    message b ls e Clean where
+      b = "String given for ID invalid"
+      ls = [ "A string you gave for a food ID is not valid."
+           , "String you gave: " ++ s
+           ]
+  
+  (NonNegIntegerStringNotValid s) ->
+    message b ls e Clean where
+      b = "String given for a non-negative integer invalid"
+      ls = [ "String you gave: " ++ s
+           ]
+  
+  (NoMoveIDsGiven) ->
+    message b ls e Clean where
+      b = "No food IDs were specified for the move command"
+      ls = []
+
+  (OneMoveIDGiven) ->
+    message b ls e Clean where
+      b = "Only one food ID was specified for the move command"
+      ls = []
+
+  (NonNegMixedNotValid s) ->
+    message b ls e Clean where
+      b = "Invalid text for a non-negative mixed number"
+      ls = [ "Invalid text given: " ++ s ]
+  
+  (PosMixedNotValid s) ->
+    message b ls e Clean where
+      b = "Invalid text for a positive mixed number"
+      ls = [ "Invalid text given: " ++ s ]
+  
+  (BoundedPercentNotValid s) ->
+    message b ls e Clean where
+      b = "Invalid text for percent"
+      ls = [ "Invalid text given: " ++ s ]
+  
+  (QByNutFail q) ->
+    message b ls e Clean where
+      b = "Could not set quantity by nutrient"
+      ls = case q of
+        QBNNoMatchingNut ->
+          [ "No nutrient was found matching the pattern given." ]
+        (QBNMultipleMatchingNuts nns) ->
+          [ "Multiple nutrients were found matching the pattern given."
+          , "Matching nutrients:"
+          ] ++ (map X.unpack . map F.unNutName $ nns)
+        (QBNNutIsZero nn) ->
+          [ "Nutrient's value is zero. Nutrient name: "
+            ++ (X.unpack . F.unNutName $ nn) ]
+  
+  (IngrToVolatileBadIdStr s) ->
+    message b ls e Clean where
+      b = "Invalid string given for ingredients-to-volatile"
+      ls = [ "String given for ingredients-to-volatile is not valid."
+           , "String given: " ++ s
+           ]
+  
+  (IngrFromVolatileBadIdStr s) ->
+    message b ls e Clean where
+      b = "Invalid string given for ingredients-from-volatile"
+      ls = [ "String given for ingredients-from-volatile is not valid."
+           , "String given: " ++ s
+           ]
+
+  (AddNutError f) ->
+    message b ls e Clean where
+      b = "Cannot add nutrient to foods whose weight is zero"
+      ls = [ "Nutrients cannot be added to foods whose weight"
+           , "is zero because Pantry cannot calculate the amount"
+           , "of nutrient the food has."
+           , "Name of food whose quantity is zero:"
+           , (let name = F.TagName . X.pack $ "name"
+              in case M.lookup name . F.getTags $ f of
+                Nothing -> "(no name)"
+                (Just n) -> X.unpack . F.unTagVal $ n)
+           , "ID of food whose quantity is zero:"
+           , (X.unpack . exact . F.getFoodId $ f)
+           ]
+  
+  (NoSortDirection s) ->
+    message b ls e Clean where
+      b = "No direction for sorting given"
+      ls = [ "The --key option requires either \"ascending\""
+             , "or \"descending\" to be specified."
+             , "Text you gave: " ++ s
+             ]
+
+  (KeyOddArguments) ->
+    message b ls e Clean where
+      b = "Odd number of arguments to the --key option given"
+      ls = [ "The --key option require an even number of arguments." ]
+  
+  (NonOptionArguments ss) ->
+    message b ls e Clean where
+      b = "Non-option arguments given"
+      ls = [ "All arguments on the Pantry command line must be"
+           , "options or arguments to options, but you gave"
+           , "non-option arguments. Non-option arguments you gave:"
+           ] ++ ss
+
+-- | Dirty indicates that after this error, files on disk may have
+-- been changed. Clean indicates that no data on disk was changed.
+data DiskState = Clean | Dirty
 
 -- | Format a standard error message. The first line will say "pantry:
 -- error:" and give a one-line summary of the message. Following lines
@@ -72,10 +364,15 @@ showError e = case e of
 message :: String -- ^ Brief description
            -> [String] -- ^ More lines
            -> Error -- ^ The error, to pull out the error code
+           -> DiskState
            -> X.Text
-message b ls e = X.unlines $ [f] ++ m ++ [l] where
+message b ls e s = X.unlines $ [f] ++ m ++ [stateStr, l] where
   f = X.pack $ "pantry: error: " ++ b
   m = map X.pack ls
+  stateStr = X.pack $ "Data in memory will remain unchanged.\n"
+             ++ case s of
+               Clean -> "No data on disk was changed."
+               Dirty -> "Data on disk may have been changed."
   l = X.pack $ "(Pantry error number " ++ show (errorCode e) ++ ")"
 
 errorCode :: Error -> Word8
